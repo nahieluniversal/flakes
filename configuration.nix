2551d6d0dcdd7db291f599f9935c90a4835ca464
nahieluniversal <olivercorreo10@gmail.com>
2025-12-27 21:11:16 +0100

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -26,8 +26,8 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 443 1401 2200 53317 ];
-  networking.firewall.allowedUDPPorts = [ 53 1194 1195 1196 1197 1300 1301 1302 1303 1400 53317 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 1401 2200 47984 47989 47990 48010 53317 53318 ];
+  networking.firewall.allowedUDPPorts = [ 53 1194 1195 1196 1197 1300 1301 1302 1303 1400 47984 47989 47990 48010 53317 53318 ];
   networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
 services.resolved = {
@@ -158,6 +158,9 @@ olivernix = {
     pkgs.android-tools
     pkgs.gamescope
     pkgs.mangohud
+    pkgs.go
+    pkgs.zoxide
+    pkgs.fzf
   ];
   programs.steam = {
   enable = true;
