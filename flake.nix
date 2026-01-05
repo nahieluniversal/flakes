0439244a5ae8000e009006fc9f4b9134383ab0f6
nahieluniversal <olivercorreo10@gmail.com>
2026-01-05 12:31:42 +0100

    deleted some comments; added hyprland.config bak

--- flake.nix
+++ flake.nix
@@ -3,13 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, zen-browser }:
     let
       system = "x86_64-linux";
@@ -17,13 +15,11 @@
         inherit system;
         config.allowUnfree = true;
       };
-
-      # Cargar el paquete directamente desde tu GitHub (¡no como flake!)
       opforjellyfin = pkgs.callPackage (pkgs.fetchFromGitHub {
         owner = "nahieluniversal";
         repo = "opforjellyfin_nix";
         rev = "28e2a785b0088d27769c186879c8643615708201";
-        sha256 = "sha256-1uODr4nVHj+nrnQx7GT3gJ6eY0GHJX0EuajdrM+1k6Q=";  # ← ¡ajusta este hash!
+        sha256 = "sha256-1uODr4nVHj+nrnQx7GT3gJ6eY0GHJX0EuajdrM+1k6Q=";
       }) { };
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -32,11 +28,8 @@
         specialArgs = {
           inherit pkgs system zen-browser ;
         };
-
         modules = [
           ./configuration.nix
-
-          # zen-browser + opforjellyfin
           ({ pkgs, system, zen-browser, ... }: {
             environment.systemPackages = [
               (zen-browser.packages.${system}.default)
