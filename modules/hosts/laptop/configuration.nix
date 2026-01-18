43e15671557987f25f5b05290bb46dab13a9b962
nahieluniversal <olivercorreo10@gmail.com>
2026-01-18 16:23:11 +0100

    updated flake.lock and changed resolved structure

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -32,11 +32,13 @@
 
 services.resolved = {
   enable = true;
-  dnssec = "true";
-  domains = [ "~." ];
-  fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-  dnsovertls = "true";
+  settings.Resolve = {
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
   };
+};
 
 
   # Set your time zone.
