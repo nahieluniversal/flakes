719427026daa2dc93b1602c0340efc42556c6606
nahieluniversal <olivercorreo10@gmail.com>
2026-01-23 22:32:46 +0100

    added qbittorrent to server

--- modules/hosts/server/system.nix
+++ modules/hosts/server/system.nix
@@ -93,6 +93,11 @@
   virtualisation.docker = {
   enable = true;
   };
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    webuiPort = 8055;
+  };
   # State version
   system.stateVersion = "26.05";
 }
