fa9e9378fe81199e045891fb5dc960a87f0626de
nahieluniversal <olivercorreo10@gmail.com>
2026-05-09 22:39:09 +0200

    nicotine autorun

--- modules/hosts/laptop/system.nix
+++ modules/hosts/laptop/system.nix
@@ -23,8 +23,8 @@
   # Networking
   networking.hostName = "laptop";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 80 443 1401 2200 8111 47984 47989 47990 48010 53317 53318 ];
-  networking.firewall.allowedUDPPorts = [ 53 1194 1195 1196 1197 1300 1301 1302 1303 1400 47984 47989 47990 48010 53317 53318 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 443 1401 2200 2234 8111 47984 47989 47990 48010 53317 53318 ];
+  networking.firewall.allowedUDPPorts = [ 53 1194 1195 1196 1197 1300 1301 1302 1303 1400 2234 47984 47989 47990 48010 53317 53318 ];
   networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
   services.resolved = {
