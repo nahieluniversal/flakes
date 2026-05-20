ad0f5b23d09eaef1f496b980d1becb08bf43a8a2
nahieluniversal <olivercorreo10@gmail.com>
2026-05-20 22:44:37 +0200

    added flaresolverr

--- modules/hosts/laptop/servers.nix
+++ modules/hosts/laptop/servers.nix
@@ -5,6 +5,7 @@
     enable = true;
     openFirewall = true;
   };
+  services.flaresolverr.enable = true;
   # To prevent shoko and jellyfin from starting  automatically
   systemd.services.shoko.wantedBy = lib.mkForce [ ];
   systemd.services.jellyfin.wantedBy = lib.mkForce [ ];
