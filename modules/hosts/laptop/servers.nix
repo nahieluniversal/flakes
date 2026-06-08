bd462878816bbc5c2f361298cfd7034d29fa1c06
nahieluniversal <olivercorreo10@gmail.com>
2026-06-08 17:07:32 +0200

    added openvpn to network manager

--- modules/hosts/laptop/servers.nix
+++ modules/hosts/laptop/servers.nix
@@ -6,8 +6,10 @@
     openFirewall = true;
   };
   services.flaresolverr.enable = true;
+  programs.openvpn3.enable = true;
+  networking.networkmanager.plugins = [pkgs.networkmanager-openvpn];
   # To prevent shoko and jellyfin from starting  automatically
-  systemd.services.shoko.wantedBy = lib.mkForce [ ];
-  systemd.services.jellyfin.wantedBy = lib.mkForce [ ];
+  #systemd.services.shoko.wantedBy = lib.mkForce [ ];
+  #systemd.services.jellyfin.wantedBy = lib.mkForce [ ];
   
 }
