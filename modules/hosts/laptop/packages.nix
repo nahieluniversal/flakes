67cdd5ad6a2946ac51fa93c42f2c873491404c62
nahieluniversal <olivercorreo10@gmail.com>
2026-06-21 12:04:23 +0200

    added bottles and flac; and another game to sls config

--- modules/hosts/laptop/packages.nix
+++ modules/hosts/laptop/packages.nix
@@ -13,6 +13,7 @@
     awww
     baobab
     brightnessctl
+    bottles
     cabextract
     calibre
     cliphist
@@ -30,6 +31,7 @@
     feh
     ffmpeg
     filezilla
+    flac
     flycast
     fzf
     gimp
